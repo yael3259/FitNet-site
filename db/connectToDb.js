518aacd905eac98539c2412b44d949225113c1a6
yael3259 <157998716+yael3259@users.noreply.github.com>
2025-04-25 16:51:03 +0300

    25/04/2025

--- db/connectToDb.js
+++ db/connectToDb.js
@@ -4,8 +4,8 @@ import mongoose from "mongoose";
 // התחברות למסד נתונים לוקאלי
 export const connectToDb = async () => {
     try {
-        let connect = await mongoose.connect(process.env.DB_URI || "mongodb://0.0.0.0:27017/fitnessDB");
-        // let connect = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        // let connect = await mongoose.connect(process.env.DB_URI || "mongodb://0.0.0.0:27017/fitnessDB");
+        let connect = await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log("mongo db connected!")
     } catch (err) {
         console.log(err);
